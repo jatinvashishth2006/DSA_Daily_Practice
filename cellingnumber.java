c755928e4ee4ab557dec158db6c6cfd780b59f72
Jatin Vashishth <jatinvashishth302@gmail.com>
2026-08-15 22:11:07 +0530

    Add files via upload

---
+++ cellingnumber.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class cellingnumber
+{
+    static int binary(int[] arr , int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(target==arr[mid])
+            {
+                return arr[mid];
+            }
+            else if (target<arr[mid])
+            {
+                end=mid-1;
+
+            }
+            else
+            {
+                start=mid+1;
+
+            }
+        }
+        return arr[start];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[10];
+        System.out.println("Enter the elemnents in an array");
+        for (int i = 0; i < 5; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target");
+        int target = sc.nextInt();
+        int ans = binary(a,target);
+        System.out.print(ans);
+    }
+}
